c789c37dd48a20cf726998d12b52f3cd8b356eaa
Hinsaul <z657723073@outlook.com>
2016-10-21 18:23:37 +0800

    Update myFEWork14.js

--- FEWork/myFEWork14.js
+++ FEWork/myFEWork14.js
@@ -12,6 +12,155 @@
 // 作业 1
 //
 // 扩充上课时候做的 slide 库, 添加一个功能, 当鼠标移动到小圆点上的时候, 播放那张图片
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>slide</title>
+        <style>
+            .vertical-center {
+                position: relative;
+                top: 50%;
+                transform: translateY(-50%);
+            }
+            .imgs-container {
+                position: relative;
+                width: 440px;
+                height: 370px;
+                overflow: hidden;
+            }
+            .image {
+                position: absolute;
+                display: none;
+            }
+            .active-image {
+                display: block;
+            }
+            .button-left {
+                position: absolute;
+                left: 0px;
+            }
+            .button-right {
+                position: absolute;
+                right: 0px;
+            }
+            .button {
+                background: lightblue;
+                opacity: 0.5;
+                color: white;
+            }
+            .active-button {
+                opacity: 1;
+                color: black;
+            }
+            .imgs-indicators-container {
+                position: absolute;
+                text-align: center;
+                width: 100%;
+                bottom: 2px;
+            }
+            .image-indicators {
+                display: inline-block;
+                background: lightgrey;
+                opacity: 0.5;
+                border-radius: 50%;
+                padding: 0 5px;
+            }
+            .active-image-indicators {
+                opacity: 1;
+                background: black;
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="imgs-container" data-index="0" data-imgs="6">
+            <img class="image active-image" src="imgs/1.jpg" alt="1.jpg" />
+            <img class="image" src="imgs/2.jpg" alt="2.jpg" />
+            <img class="image" src="imgs/3.jpg" alt="3.jpg" />
+            <img class="image" src="imgs/4-1.jpg" alt="4.jpg" />
+            <img class="image" src="imgs/5-1.jpg" alt="5.jpg" />
+            <img class="image" src="imgs/6.jpg" alt="6.jpg" />
+            <button class="button-left vertical-center button" type="button" name="button">Pre</button>
+            <button class="button-right vertical-center button" type="button" name="button">Next</button>
+
+            <div class="imgs-indicators-container">
+                <div class="image-indicators active-image-indicators">1</div>
+                <div class="image-indicators">2</div>
+                <div class="image-indicators">3</div>
+                <div class="image-indicators">4</div>
+                <div class="image-indicators">5</div>
+                <div class="image-indicators">6</div>
+            </div>
+        </div>
+
+        <script src="http://cdn.bootcss.com/jquery/3.1.1/jquery.js"></script>
+        <script type="text/javascript">
+            var indexOfElement = function(element) {
+                var parent = element.parentElement
+                for (var i = 0; i < parent.children.length; i++) {
+                    var e = parent.children[i]
+                    if (e === element) {
+                        return i
+                    }
+                }
+            }
+
+            var play = function(b) {
+                $('.active-image').removeClass('active-image')
+                $('.active-image-indicators').removeClass('active-image-indicators')
+                //
+                var index = $('.imgs-container').data('index')
+                // console.log(index)
+                var imgs = $('.imgs-container').data('imgs')
+                // console.log(imgs)
+                var i = (index + b + imgs) % imgs
+                // console.log(i)
+                $('.imgs-container').data('index', i)
+                //
+                var activeImage = $($('.image')[i])
+                activeImage.addClass('active-image')
+                //
+                var activeIndicator = $($('.image-indicators')[i])
+                activeIndicator.addClass('active-image-indicators')
+            }
+
+            var playIndicators = function(activeI) {
+                $('.active-image').removeClass('active-image')
+                $('.active-image-indicators').removeClass('active-image-indicators')
+
+                // var activeI = indexOfElement(event.target)
+                // console.log(activeI)
+                $('.imgs-container').data('index', activeI)
+                //
+                var activeImage = $($('.image')[activeI])
+                activeImage.addClass('active-image')
+                //
+                var activeIndicator = $($('.image-indicators')[activeI])
+                activeIndicator.addClass('active-image-indicators')
+            }
+
+            $('.button').on('click', function(event) {
+                if ($(event.target).hasClass('button-left')) {
+                    play(-1)
+                } else {
+                    play(1)
+                }
+            })
+            // $('.image-indicators').on('click', function(event) {
+            //     var activeI = indexOfElement(event.target)
+            //     playIndicators(activeI)
+            // })
+            $('.image-indicators').on('mouseover', function(event) {
+                var activeI = indexOfElement(event.target)
+                playIndicators(activeI)
+            })
+        </script>
+    </body>
+</html>
+
+
+
 
 
 // 作业 2
@@ -19,12 +168,199 @@
 // 扩充上课时候做的 slide 库, 添加一个功能, 当鼠标点击小圆点的时候, 播放那张图片
 
 
+
+
 // 作业 3
 //
 // 扩充上课时候做的 slide 库, 添加一个功能
 // 小圆点被一系列缩略图取代
 // 鼠标点击缩略图播放那张图片
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>slide-3</title>
+        <style>
+            .vertical-center {
+                position: relative;
+                top: 50%;
+                transform: translateY(-50%);
+            }
+            .imgs-container {
+                position: relative;
+                width: 462px;
+                height: 370px;
+                overflow: hidden;
+            }
+            .image {
+                position: absolute;
+                display: none;
+            }
+            .active-image {
+                display: block;
+            }
+            .button-left {
+                position: absolute;
+                left: 0px;
+            }
+            .button-right {
+                position: absolute;
+                right: 0px;
+            }
+            .button {
+                background: lightblue;
+                opacity: 0.5;
+                color: white;
+            }
+            .active-button {
+                opacity: 1;
+                color: black;
+            }
+            .imgs-indicators-container {
+                position: relative;
+                text-align: center;
+                width: 440px;
+                bottom: 2px;
+            }
+            .image-indicators {
+                display: inline-block;
+                background: lightgrey;
+                opacity: 0.5;
+                border-radius: 50%;
+                padding: 0 5px;
+            }
+            .active-image-indicators {
+                opacity: 1;
+                background: black;
+                color: white;
+            }
+            .image-ab {
+                position: relative;
+                overflow: hidden;
+                display: inline-block;
+                bottom: -6px;
+                background: lightblue;
+                width: 80px;
+                opacity: 0.5;
+            }
+            .active-image-ab {
+                opacity: 1;
+            }
+            .ab {
+                display: block;
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="imgs-container" data-index="0" data-imgs="6">
+            <img class="image active-image" src="imgs/1.jpg" alt="1.jpg" />
+            <img class="image" src="imgs/2.jpg" alt="2.jpg" />
+            <img class="image" src="imgs/3.jpg" alt="3.jpg" />
+            <img class="image" src="imgs/4-1.jpg" alt="4.jpg" />
+            <img class="image" src="imgs/5-1.jpg" alt="5.jpg" />
+            <img class="image" src="imgs/6.jpg" alt="6.jpg" />
+            <button class="button-left vertical-center button" type="button" name="button">Pre</button>
+            <button class="button-right vertical-center button" type="button" name="button">Next</button>
+        </div>
 
+        <!-- <div class="imgs-indicators-container">
+            <div class="image-indicators active-image-indicators"><img src="images/1-s.jpg" alt="" /></div>
+            <div class="image-indicators">2</div>
+            <div class="image-indicators">3</div>
+            <div class="image-indicators">4</div>
+            <div class="image-indicators">5</div>
+            <div class="image-indicators">6</div>
+        </div> -->
+
+        <div class="image-ab-container">
+            <div class="image-ab active-image-ab">
+                <img class="ab" src="imgs/ab1.jpg" alt="" />
+            </div>
+            <div class="image-ab">
+                <img class="ab" src="imgs/ab2.jpg" alt="" />
+            </div>
+            <div class="image-ab">
+                <img class="ab" src="imgs/ab3.jpg" alt="" />
+            </div>
+            <div class="image-ab">
+                <img class="ab" src="imgs/ab4.jpg" alt="" />
+            </div>
+            <div class="image-ab">
+                <img class="ab" src="imgs/ab5.jpg" alt="" />
+            </div>
+            <div class="image-ab">
+                <img class="ab" src="imgs/ab6.jpg" alt="" />
+            </div>
+        </div>
+
+        <script src="http://cdn.bootcss.com/jquery/3.1.1/jquery.js"></script>
+        <script type="text/javascript">
+            var indexOfElement = function(element) {
+                var parent = element.parentElement
+                for (var i = 0; i < parent.children.length; i++) {
+                    var e = parent.children[i]
+                    if (e === element) {
+                        return i
+                    }
+                }
+            }
+
+            var play = function(b) {
+                $('.active-image').removeClass('active-image')
+                $('.active-image-ab').removeClass('active-image-ab')
+                //
+                var index = $('.imgs-container').data('index')
+                // console.log(index)
+                var imgs = $('.imgs-container').data('imgs')
+                // console.log(imgs)
+                var i = (index + b + imgs) % imgs
+                // console.log(i)
+                $('.imgs-container').data('index', i)
+                //
+                var activeImage = $($('.image')[i])
+                activeImage.addClass('active-image')
+                //
+                var activeIndicator = $($('.image-ab')[i])
+                activeIndicator.addClass('active-image-ab')
+            }
+
+            var playIndicators = function(activeI) {
+                $('.active-image').removeClass('active-image')
+                $('.active-image-ab').removeClass('active-image-ab')
+
+                // var activeI = indexOfElement(event.target)
+                // console.log(activeI)
+                $('.imgs-container').data('index', activeI)
+                //
+                var activeImage = $($('.image')[activeI])
+                activeImage.addClass('active-image')
+                //
+                var activeIndicator = $($('.image-ab')[activeI])
+                activeIndicator.addClass('active-image-ab')
+            }
+
+            $('.button').on('click', function(event) {
+                if ($(event.target).hasClass('button-left')) {
+                    play(-1)
+                } else {
+                    play(1)
+                }
+            })
+            // $('.image-indicators').on('click', function(event) {
+            //     var activeI = indexOfElement(event.target)
+            //     playIndicators(activeI)
+            // })
+            $('.image-ab').on('click', function(event) {
+                var activeI = indexOfElement(event.target.parentElement)
+                playIndicators(activeI)
+            })
+        </script>
+    </body>
+</html>
+              
+                
+                
 
 // 作业 4
 // 函数封装
